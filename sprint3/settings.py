6c4cbf00c3799184f14f5e58e22d233e427bf91f
Daniel Fuentes <110946834+Soulho@users.noreply.github.com>
2024-05-03 16:53:48 -0500

    Update settings.py

--- sprint3/settings.py
+++ sprint3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ho34%bvhn)tw&h@wsdg5+_cxnhpan&9k(++4z*qiw*#d)nj4d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
